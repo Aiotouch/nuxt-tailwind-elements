2c0c3be2112894beb58489774f63e992f9bebe3c
Jobbers Connect <myjobbersconnect@gmail.com>
2022-04-18 21:04:59 +0100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
+}
